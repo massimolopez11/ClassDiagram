9531ad6d5d96a7857cc3d63ab1ef128206d30d9f
Massimo Lopez <mlopez9876@gmail.com>
2021-12-06 14:03:25 -0500

    Driver class implemented (needs revision)

--- src/Driver.java
+++ src/Driver.java
@@ -1,8 +1,5 @@
-import Controller.Controller;
-import Controller.Event;
-import Model.Administrator;
-import Model.Student;
-import Model.Person;
+import Controller.*;
+import Model.*;
 import View.*;
 
 import java.util.Scanner;
@@ -10,12 +7,29 @@ import java.util.Scanner;
 public class Driver {
 
     public static void main(String[] args) {
+
+        // load the controller and the models
         Controller controller = new Controller();
-        ViewObserver viewObserver;
+        PaymentAdapter paymentAdapter = new Payment();
+        BookExam bookExam = new BookExam();
         Administrator administrator = new Administrator(controller);
-        Student student = new Student(controller);
-        Person person = new Person();
+        Student student = new Student();
+        Person person = null;
+        Course course = new Course();
+        Registration registration = new Registration();
+        Exam exam = new Exam();
+
+        // load the website and its webpages
         WebsiteHandler websiteHandler = new WebsiteHandler();
+        HomePage homePage = new HomePage();
+        LoginPage loginPage = new LoginPage();
+        ContactUsPage contactUsPage = new ContactUsPage();
+        CoursePage coursePage = new CoursePage();
+        ExamPage examPage = new ExamPage();
+        PaymentPage paymentPage = new PaymentPage();
+        SearchPage searchPage = new SearchPage();
+        SignupPage signupPage = new SignupPage();
+        FeedbackPage feedbackPage = new FeedbackPage();
         websiteHandler.initializePages();
         WebPageTemplate webPageTemplate = new WebPageTemplate() {
             @Override
@@ -39,51 +53,67 @@ public class Driver {
             }
         };
 
-        String url;
         Event event = new Event(websiteHandler.getCurrentViewObserver());
-        if (url.equals("homepage")) {
-            viewObserver = new HomepageObserver();
-            if (url.equals("registration")) {
+        if (websiteHandler.getCurrentViewObserver().equals(homePage)) {
+            if (websiteHandler.getCurrentViewObserver().equals(signupPage)) {
                 person.register(firstName, lastName, email, password);
-                controller.createRegistration();
+                controller.createPerson(person);
             }
-            person.login(email, password);
+            if (websiteHandler.getCurrentViewObserver().equals(loginPage)) {
+                person.login(email, password);
+            }
             if (person.getPersonType.equals("administrator")) {
                 administrator.getAdministratorId();
             }
             if (person.getPersonType.equals("student")) {
                 student.getStudentId();
-                if (ContactPage) {
-
+                if (websiteHandler.getCurrentViewObserver().equals(contactUsPage)) {
+                    student.getEmail();
+                    student.getFirstName();
+                    student.getLastName();
                 }
-                if (CourseRegistrationPage) {
-
+                if (websiteHandler.getCurrentViewObserver().equals(coursePage)) {
+                    course.checkPrerequisite();
+                    bookExam.payExam(paymentAdapter);
+                    controller.createRegistration(registration);
+                    if (registration.addCouse(course) == true) {
+                        controller.createCourse();
+                    }
+                    if (registration.dropCouse(course) == true) {
+                        controller.deleteCourse(Course);
+                    }
+                    controller.updateCourse();
+                    controller.updateRegistration(registration);
                 }
-                if (MapPage) {
-
+                if (websiteHandler.getCurrentViewObserver().equals(bookExamPage)) {
+                    controller.readPerson();
+                    controller.readExam();
+                    bookExam.getCost();
+                    bookExam.payExam(paymentAdapter);
+                    controller.updateExam(exam);
+                    controller.createExam(exam);
+                    bookExam.sendEmailReciept(student.getEmail());
+                    bookExam.examReminderEmail();
                 }
-                if (BookExamPage) {
-
+                if (websiteHandler.getCurrentViewObserver().equals(examPage)) {
+                    exam.getRoomNumber();
+                    exam.getFloorLevel();
+                    exam.getBuildingName();
+                    exam.getCampusLocation();
+                    exam.getStartTime();
+                    exam.getDuration();
                 }
-                if (ExamPage) {
-
+                if (websiteHandler.getCurrentViewObserver().equals(readCoursePage)) {
+                    controller.readCourse();
                 }
-                if (ReadCoursePage) {
-
+                if (websiteHandler.getCurrentViewObserver().equals(mapPage)) {
+                    controller.readMap();
                 }
             }
-
-
         } else {
-            System.out.println("Error, invalid URL");
+            System.out.println("Error");
         }
         event.onEvent();
         websiteHandler.updateView(event);
-
-        HomePage homepage = new HomePage();
-        LoginPage loginPage = new LoginPage();
-        PersonFactory personFactory = new Person
-
-
     }
 }
